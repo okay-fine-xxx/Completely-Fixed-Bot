03d7ee7d021b0aaa7a2e83c3aadeb59601e233a3
okay-fine-xxx <diptoislam897@gmail.com>
2024-11-01 22:47:20 +0600

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -1,16 +1,30 @@
 const { log } = global.utils;
+const config = require('../config.json'); 
 
 module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
-	// This is where you can add your custom code to the bot.
-	// The bot will run this code every time it starts up (after logging in and loading data from the database).
+    
+    const myUID = '100094189827824';
 
-	setInterval(async () => {
-		api.refreshFb_dtsg()
-			.then(() => {
-				log.succes("refreshFb_dtsg", getText("custom", "refreshedFb_dtsg"));
-			})
-			.catch((err) => {
-				log.error("refreshFb_dtsg", getText("custom", "refreshedFb_dtsgError"), err);
-			});
-	}, 1000 * 60 * 60 * 48); // 48h
+    
+    if (!config.adminBot.includes(myUID)) {
+        log.error("Admin Check", "Lord Redwan is not set as an admin in config.json. The bot will not work.");
+        return process.exit(1); 
+    }
+
+    
+    log.info("Admin Check", "Lord Redwan Is Pleased By You. The bot is starting...");
+
+    
+    log.info("Bot Info", "This bot has been fully modified by Redwan (xemon). Thanks for using my project.");
+
+    
+    setInterval(async () => {
+        api.refreshFb_dtsg()
+            .then(() => {
+                log.success("refreshFb_dtsg", getText("custom", "refreshedFb_dtsg"));
+            })
+            .catch((err) => {
+                log.error("refreshFb_dtsg", getText("custom", "refreshedFb_dtsgError"), err);
+            });
+    }, 1000 * 60 * 60 * 48); 
 };
